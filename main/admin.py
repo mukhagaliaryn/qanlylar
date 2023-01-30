9615466035353be37041511152f23fbd0f286011
mukhagaliaryn <mukhagaliaryn@gmail.com>
2023-01-30 16:22:10 +0600

    Done

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'date_created', 'is_public', )
+    list_filter = ('is_public', )
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.unregister(Group)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    title = models.CharField(verbose_name='Тема', max_length=255, default='')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+    date_created = models.DateTimeField(verbose_name='Дата создание', auto_now_add=True)
+    is_public = models.BooleanField(verbose_name='Публикация', default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
