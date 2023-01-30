e6ce74383da3b11729ad3cd016717c9c88045d78
mukhagaliaryn <mukhagaliaryn@gmail.com>
2023-01-30 21:34:28 +0600

    Done

--- templates/static/tailwind/tailwind.config.js
+++ templates/static/tailwind/tailwind.config.js
@@ -2,13 +2,12 @@
 module.exports = {
   content: [
     '../../**/*.html',
-    './node_modules/flowbite/**/*.js',
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     fontFamily: {
       inter: ['Inter Tight', 'sans-serif'],
     },
-
     extend: {},
   },
   plugins: [
