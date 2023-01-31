6c7b1033da40aee4bc789846080a097191e88bb5
mukhagaliaryn <mukhagaliaryn@gmai.com>
2023-01-31 15:19:05 +0300

    Done

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,30 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('historic/', views.historic, name='historic'),
+    path('legacy/', views.legacy, name='legacy'),
+    path('reviews/', views.reviews, name='reviews'),
+    path('books/', views.books, name='books'),
+
+
+    path('mausoleum/', views.mausoleum, name='mausoleum'),
+    path('persons/', views.persons, name='persons'),
+    path('department_of_mothers/', views.department_of_mothers, name='department_of_mothers'),
+
+
+    path('grand_words/', views.grand_words, name='grand_words'),
+    path('termes/', views.termes, name='termes'),
+    path('songs/', views.songs, name='songs'),
+    path('states/', views.states, name='states'),
+    path('literature/', views.literature, name='literature'),
+
+
+    path('chronicle/', views.chronicle, name='chronicle'),
+
+
+    path('charity/', views.charity, name='charity'),
+    path('articles/', views.articles, name='articles'),
+    path('children/', views.children, name='children'),
+    path('mediagallery/', views.mediagallery, name='mediagallery'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,86 @@
 from django.shortcuts import render
 
 
+# Басты бет
 def index(request):
     return render(request, 'main/index.html', {})
+
+
+# Тарихи деректер
+# ====================================================================================================================
+def historic(request):
+    return render(request, 'main/historic/index.html', {})
+
+
+def legacy(request):
+    return render(request, 'main/historic/legacy.html', {})
+
+
+def reviews(request):
+    return render(request, 'main/historic/reviews.html', {})
+
+
+def books(request):
+    return render(request, 'main/historic/books.html', {})
+
+
+# Тағылымдар
+# ====================================================================================================================
+def mausoleum(request):
+    return render(request, 'main/teaching/mausoleum.html', {})
+
+
+def persons(request):
+    return render(request, 'main/teaching/persons.html', {})
+
+
+def department_of_mothers(request):
+    return render(request, 'main/teaching/department_of_mothers.html', {})
+
+
+# Руханият
+# ====================================================================================================================
+def grand_words(request):
+    return render(request, 'main/spirituality/grand_words.html', {})
+
+
+def termes(request):
+    return render(request, 'main/spirituality/termes.html', {})
+
+
+def songs(request):
+    return render(request, 'main/spirituality/songs.html', {})
+
+
+def states(request):
+    return render(request, 'main/spirituality/states.html', {})
+
+
+def literature(request):
+    return render(request, 'main/spirituality/literature.html', {})
+
+
+# Шежіре
+# ====================================================================================================================
+def chronicle(request):
+    return render(request, 'main/chronicle/index.html', {})
+
+
+# Қайырымдылық
+def charity(request):
+    return render(request, 'main/charity.html', {})
+
+
+# Мақалалар
+def articles(request):
+    return render(request, 'main/articles.html', {})
+
+
+# Балалар бөлімі
+def children(request):
+    return render(request, 'main/children.html', {})
+
+
+# Медиагалерея
+def mediagallery(request):
+    return render(request, 'main/mediagallery.html', {})
